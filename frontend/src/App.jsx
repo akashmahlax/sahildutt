01d53388d2f2021fdfad2274ed79e0486d59b199
akashmahlax <akashdalla407@gmail.com>
2024-11-23 21:52:07 +0530

    tittle changed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,4 @@
 // App.js
-<<<<<<< HEAD
 import React, { useState } from "react";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
@@ -37,32 +36,4 @@ const App = () => {
   );
 };
 
-=======
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
-import Hero from "./components/Hero";
-import About from "./pages/About";
-import Services from "./pages/Services";
-import Portfolio from "./pages/Portfolio";
-import Testimonials from "./pages/Testimonials";
-import Contact from "./pages/Contact";
-
-const App = () => (
-  <Router>
-    <Navbar />
-    <Routes>
-      <Route path="/" element={<Hero />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/services" element={<Services />} />
-      <Route path="/portfolio" element={<Portfolio />} />
-      <Route path="/testimonials" element={<Testimonials />} />
-      <Route path="/contact" element={<Contact />} />
-    </Routes>
-    <Footer />
-  </Router>
-);
-
->>>>>>> 9db45834d41818613897a13df2d4cbdbebcc2fc4
 export default App;
